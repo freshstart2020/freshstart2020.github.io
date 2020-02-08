dcb28f500377f0630a3bcf0a8b6044176c79e47e
freshstart2020 <60795133+freshstart2020@users.noreply.github.com>
2020-02-07 23:53:21 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -78,7 +78,20 @@
 				});
 
 		// Header.
-			
+			if (skel.vars.IEVersion < 9)
+				$header.removeClass('alt');
+
+			if ($banner.length > 0
+			&&	$header.hasClass('alt')) {
+
+				$window.on('resize', function() { $window.trigger('scroll'); });
+
+				$banner.scrollex({
+					bottom:		$header.outerHeight() + 1,
+					terminate:	function() { $header.removeClass('alt'); },
+					enter:		function() { $header.addClass('alt'); },
+					leave:		function() { $header.removeClass('alt'); }
+				});
 
 			}
 
